99f0d2c7a761a1619efae4edb2fdd01ea32f9e22
Noel Betanzos De La Cruz <122043746@upq.edu.mx>
2025-03-30 14:59:23 -0600

    Update register.php

--- register.php
+++ register.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     //$hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
     // Insertar usuario en la base de datos
-    $sql = "INSERT INTO usuarios (nombre, telefono, correo, contraseña, id_rol_usuario) VALUES ('$username', '$phone', '$email', '$password', '3')";
+    $sql = "INSERT INTO Usuarios (nombre, telefono, correo, contraseña, id_rol_usuario) VALUES ('$username', '$phone', '$email', '$password', '2')";
     //echo "Consulta SQL: $sql<br>";
 
     if ($conn->query($sql) === TRUE) {
