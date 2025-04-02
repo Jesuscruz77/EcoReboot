b0de5e33c94b017c213712bb6347f3ffa85fa2ae
Jesuscruz77 <122043768@upq.edu.mx>
2025-04-02 07:06:39 -0600

    Proyecto al 90%
    Proyecto terminado, solo falta conectar la api al crud

--- ecoreboot.sql
+++ ecoreboot.sql
@@ -89,14 +89,8 @@ CREATE TABLE `donaciones` (
 INSERT INTO `donaciones` (`id_donacion`, `id_usuario`, `id_tipo_electrodomestico`, `id_estado_dispositivo`, `fecha`, `imperfecciones`, `telefono`, `total_dispositivos`, `activo`) VALUES
 (1, 3, 1, 1, '2024-01-15', 'Ninguna', '5554567890', 3, 1),
 (2, 3, 2, 2, '2024-02-10', 'Pantalla rota', '5554567890', 1, 1),
-(3, 28, 3, 2, '2024-08-10', 'Falta de botones', '4481028382', 1, 0),
-(4, 2, 5, 1, '2024-08-10', 'Ninguno', '4421389563', 1, 0),
-(5, 5, 4, 1, '2024-08-03', 'Funciona perfectamente', '4481028843', 1, 1),
-(6, 9, 2, 1, '2024-08-01', 'Ninguno', '4481228823', 1, 1),
-(7, 3, 1, 2, '2024-07-18', 'No sirve la bateria', '4421119563', 1, 1),
-(8, 3, 1, 2, '2024-08-02', 'No sirve la tarjeta madre', '4271564332', 1, 0),
-(9, 5, 2, 1, '2024-08-03', 'Esta en perfecto estado ', '4481042277', 1, 1),
-(10, 5, 1, 2, '2024-08-12', 'No funciona la bateria ', '4481028843', 2, 1);
+(3, 2, 5, 1, '2024-08-10', 'Ninguno', '4421389563', 1, 0),
+
 
 -- --------------------------------------------------------
 
@@ -322,64 +316,12 @@ INSERT INTO `usuarios` (`id_usuario`, `nombre`, `telefono`, `correo`, `contrase√
 (1, 'Admin01', '4481028843', 'admin01@gmail.com', '123456789', 1),
 (2, 'MariaLopez', '555555', '5559876543', 'maria.lopez@gmail.com', 2),
 (3, 'CarlosGomez', '44444', '5554567890', 'carlos.gomez@gmail.com', 3),
-(4, 'usuario2', '4417327450', 'user.4@gmail.com', 'lshhyr45', 3),
-(5, 'mgfuentes', '4481028843', 'mariaguadalupefuentes9917@gmail.com', 'lupita291016', 3),
-(6, 'MAabd', '4481022746', 'asdfg@jvbkhjl', '$2y$10$04psEqtZaW9D.T1DraRQG.XmoZMk.ItJsjyYKJIQYeEdfflzjrmBG', 3),
-(7, 'Usuario4', '442184390', 'usne4@gmail.com', '$2y$10$ZYTRYGgVZgMbeNvHwo5i2OMhOLFI9lV5H/8EJWJgb8RjtQkFX6Rae', 3),
-(8, 'Usuario3', '4481023333', 'usnnbb3@gmail.com', '$2y$10$x5eY0dD47t5tuWqYwgsfy.3PWc9D1BTCxsPnnNzMSshWqxiysITI.', 3),
-(9, 'Yuliana', '4421225723', 'yulid24@gmail.com', '$2y$10$ruOTNww8.wcNeF/2gAyfeODIHUwgZx5fAbl58vyA2xv8pw7OIqFvq', 3),
-(10, 'Usuario5', '4481056983', 'usnnb_l5@gmail.com', '$2y$10$di3rAoocMRhc0vSun0VIgehCmA7Y78d2KuaZRJNRXqU2pIR/OYmWe', 3),
-(11, '122042900@upq.edu.mx', '4444444444', '122041950@upq.edu.mx', '$2y$10$E2VXnGDxOAewp2zXC8u9Z.YjJcP9XC4P.jmK85s5jmjkd74qVgWEG', 3),
-(12, 'dfghnjm', '4481022746', 'asdfg@jvbkhjl', '$2y$10$fyJZUp87bAN8sQlYub3SRO1S34j5IHbuRjZWv9xzNUtZb50RtPnJy', 3),
-(13, 'xdxdxdxdxdxdx', '1234567899', 'dxdxdx@gmail.com', '$2y$10$0jzS9UHMAzD6MGaEGcHT6ev4k6qRhrkDmqh0o5yzWswcFWwHjL8OO', 3),
-(14, 'ffffffff', '1234567611', 'sdfghfdsasd@gmail.com', '$2y$10$n8CBWxTy.6/thyjOqyfTO.Nt.SMhtX0MwNpF3t/GPNaVeoB/xEL6G', 3),
-(15, 'chava', '4441117643', 'chava@gmail.con', '$2y$10$juWtj4/FbNzRigCvyItryOp3gAxgO8L1jDgP4DokKV4vZQo1NgHNW', 3),
-(16, 'yyyyyyyyyy', '1111111111', 'yyyyyyy@gmail.com', '$2y$10$iF.mAcmsTIh.UMOi4DOSsOQukeHbp.nibSxYlAg8G6CDxDmDB3/SG', 3),
-(17, 'yyyyyyyyyy', '1111111111', 'yyyyyyy@gmail.com', '$2y$10$Cq3D4F/iSeMixzGSKh9mp.20lGGUSS2cX0juoFVCgYsvEI.7172oW', 3),
-(18, 'mariaguadalupefuentes9917@gmail.com', '4481028843', 'mariaguadalupefuentes9917@gmail.com', '$2y$10$qqtJ2yO0IrzRc3bZqCwdBul/cTlfYTYgN30EOfEkoKEoi3tU7gQae', 3),
-(19, 'mgfuentessssssssssssssssss', '4444444444', '122041950@upq.edu.mx', '$2y$10$kwqL1dsrirv/qyhfT5tKi.pq/DFV6TdtGCApT/lS4EJMEmbL3lkE6', 3),
-(20, 'tttttttttttttttttttt', '4481022746', 'mariaguadalupefuentes9917@gmail.com', '$2y$10$nzq5S6xr3WkleZdcgzW17eLsLBvdxdV1GROfet6J2F0O5nPlZJPKO', 3),
-(21, 'mgggggggggggggggggfuentes', '4481023333', 'fuentesmg09@gmail.com', '123456', 3),
-(22, 'traka', '4421225723', 'traka@gmail.com', 'traka', 3),
-(23, 'USUARIO10', '4481023333', 'USUARIO10@GMAIL.COM', 'USUARIO10', 3),
-(24, 'Billie', '4481028843', 'Billie@gmail.com', 'Billie', 3),
-(25, 'chucho', '4481023333', 'chucho@gmail.com', 'chucho12', 3),
-(26, 'eaaaaaaaaaa', '4421225723', 'eaaaaaaaaaaaaaa@gmail.com', '12345', 3),
-(27, 'aaaaaaaaaaaaaaa', '4481028843', 'fuentesmg09@gmail.com', 'aaaaaaaaaaaaaaa', 3),
-(28, 'DOMI', '4444444444', 'DOMI@gmail.com', 'patito', 3),
-(29, 'fernando', '4481028843', 'ferns82@gmail.com', '12345', 3),
-(30, 'polima', '4421225722', 'polima@gmail.com', '12345', 3),
-(31, 'polima2', '4481022746', 'polima2@gmail.com', '11111', 3);
+
 
 --
 -- Disparadores `usuarios`
 --
-DELIMITER $$
-CREATE TRIGGER `ValidarUsuarioUnico` BEFORE INSERT ON `usuarios` FOR EACH ROW BEGIN
-    DECLARE v_count INT;
 
-    -- Verificar si ya existe un usuario con el mismo nombre
-    SELECT COUNT(*)
-    INTO v_count
-    FROM Usuarios
-    WHERE nombre = NEW.nombre;
-
-    IF v_count > 0 THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El nombre de usuario ya existe.';
-    END IF;
-
-    -- Verificar si ya existe un usuario con el mismo correo
-    SELECT COUNT(*)
-    INTO v_count
-    FROM Usuarios
-    WHERE correo = NEW.correo;
-
-    IF v_count > 0 THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El correo del usuario ya existe.';
-    END IF;
-END
-$$
-DELIMITER ;
 
 --
 -- √çndices para tablas volcadas
